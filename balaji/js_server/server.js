25ec6f1a9d0abf53a76bcc2f9dd1db23ef7f9e72
gis4 <gis4@mssrf.res.in>
2019-04-09 16:05:32 +0530

    java over

--- balaji/js_server/server.js
+++ balaji/js_server/server.js
@@ -18,7 +18,31 @@ app.get('/add', (req, res) => {
     	'result':num2+num1
     });
 });
+app.get('/subtract', (req, res) => {
 
+    var num1=req.query.num1;
+    num1=Number(num1);
+
+    var num2=req.query.num2;
+    num2=Number(num2);
+
+    res.send({
+        'result':num2-num1
+    });
+});
+
+app.get('/multiply', (req, res) => {
+
+    var num1=req.query.num1;
+    num1=Number(num1);
+
+    var num2=req.query.num2;
+    num2=Number(num2);
+
+    res.send({
+        'nagaraj_result':num2*num1
+    });
+});
 app.post('/greatest_square', (req, res) => {
 
 	var num1=req.body.num1;
